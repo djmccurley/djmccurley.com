56ce4c342825718265a42bcfd04e1f68d1426a3f
Dan McCurley <djmccurley@gmail.com>
2016-10-25 12:49:48 -0500

    mobile menu working correctly

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function menuToggle() {
+	var bodyState = document.getElementById("body");
+	if(bodyState.className != "show_menu") {
+		bodyState.className = "show_menu";
+		console.log("Menu open");
+	} else {
+		bodyState.className = "";
+		console.log("menu closed");
+	}
+}
+
+document.getElementById("menu_button").addEventListener("click", menuToggle);
